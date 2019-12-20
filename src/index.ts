f0d8fdcc7dbe954dcf618e63ee439829e9a2c449
Taiwo Orilogbon <taiwo@bundle.africa>
2019-12-20 18:54:42 +0100

    Several Improvements to Action
    * Clients can specify if they want the parameters to be decrypted or now.
    * Bug Fix: Fixed truncation of SSM parameters, previously limited to 10

--- src/index.ts
+++ src/index.ts
@@ -2,48 +2,56 @@ import * as core from '@actions/core'
 import * as AWS from 'aws-sdk'
 import {formatter} from './format'
 import {writeFileSync} from 'fs'
-import {Parameter} from 'aws-sdk/clients/ssm'
+import {GetParametersByPathResult, Parameter} from 'aws-sdk/clients/ssm'
 
 
 async function run() {
-  const region = process.env.AWS_DEFAULT_REGION
-  const ssm = new AWS.SSM({region})
-
-  try {
-    const ssmPath = core.getInput('ssm-path', {required: true})
-    const format = core.getInput('format', {required: true})
-    const output = core.getInput('output', {required: true})
-    const prefix = core.getInput('prefix')
+    const region = process.env.AWS_DEFAULT_REGION;
+    const ssm = new AWS.SSM({region});
 
     try {
-      const {Parameters} = await ssm
-        .getParametersByPath({
-          Path: ssmPath,
-          Recursive: true,
-        })
-        .promise()
+        const ssmPath = core.getInput('ssm-path', {required: true});
+        const format = core.getInput('format', {required: true});
+        const output = core.getInput('output', {required: true});
+        const prefix = core.getInput('prefix');
 
-      core.debug(`parameters length: ${Parameters.length}`)
-      core.debug(JSON.stringify(Parameters.map(p => p.Name)))
+        let Next: string = null;
+        const allParameters: Parameter[] = [];
+        const withDecryption: boolean = core.getInput('decryption') !== 'false';
+        try {
+            do {
+                const result: GetParametersByPathResult = await ssm
+                    .getParametersByPath({
+                        WithDecryption: withDecryption,
+                        Path: ssmPath,
+                        NextToken: Next,
+                        Recursive: true,
+                    })
+                    .promise();
 
-      const envs = Parameters
-        .map<Parameter>(p => ({
-          ...p,
-          Name: p.Name.split('/').pop(),
-        }))
-        .map<string>(formatter(format)(prefix))
-      if (envs.length > 0) {
-        envs.push('\n')
-      }
+                core.debug(`parameters length: ${result.Parameters.length}`);
+                Next = result.NextToken;
+                allParameters.push(...result.Parameters);
+            } while (Next);
 
-      writeFileSync(output, envs.join('\n'))
+
+            const envs = allParameters.map<Parameter>(p => ({
+                Value: p.Value,
+                Name: p.Name.split('/').pop(),
+            }))
+                .map<string>(formatter(format)(prefix));
+            if (envs.length > 0) {
+                envs.push('\n')
+            }
+
+            writeFileSync(output, envs.join('\n'))
+        } catch (e) {
+            core.error(e);
+            core.setFailed(e.message)
+        }
     } catch (e) {
-      core.error(e)
-      core.setFailed(e.message)
+        core.setFailed(e.message)
     }
-  } catch (e) {
-    core.setFailed(e.message)
-  }
 }
 
-run()
+run();
